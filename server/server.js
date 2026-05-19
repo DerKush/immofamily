dfaa5144506779cdbd86be0e87d0487725031367
Kader <mhaidara1997@gmail.com>
2026-05-19 23:53:17 +0100

    Correction CORS

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Middleware
-app.use(cors({ origin: ['https://immofamily.onrender.com','http://localhost:5173'], credentials: true }));
+app.use(cors({ origin: ['https://immomoulaye.netlify.app','http://localhost:5173'], credentials: true }));
 app.use(express.json());
 
 // Routes API
